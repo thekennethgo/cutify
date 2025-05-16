d79b232135ab76d3205cdfcfd1d4ded29b186765
kennethgo <gennethko@gmail.com>
2025-05-16 21:32:27 +1000

    Realised it is illegal to donwload mp3 from Youtube :(, will change project direction

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,6 @@ import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const [selectedSong, setSelectedSong] = useState(null);
-  const router = useRouter();
-
-  const cutSong = () => {    
-    router.push(`/cut`);
-  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -21,8 +16,6 @@ export default function Home() {
               <h2 className="text-xl font-semibold">{selectedSong.title}</h2>
               <p className="text-gray-600">{selectedSong.artist}</p>
             </div>
-
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={cutSong}> Cut song </button>
           </div>
         )}
         <SearchBar onSongSelect={setSelectedSong} />
